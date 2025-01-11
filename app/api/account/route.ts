5134bb51622cc7c41afc36668c4a2ef6a438338f
olamgir95 <ollomurodovolamgir233@gmail.com>
2025-01-11 17:04:40 +0900

    Get all account and Delete Api

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+// Create an account
 export async function POST(req: Request) {
   try {
     await connectToDatabase();
@@ -32,7 +33,60 @@ export async function POST(req: Request) {
     const hashPin = await hash(pin, 10);
     const account = await Account.create({ name, pin: hashPin, uid });
 
-    return NextResponse.json({ account });
+    return NextResponse.json({ success: true, account });
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+}
+
+// Get all acount
+export async function GET(req: Request) {
+  try {
+    await connectToDatabase();
+    const { searchParams } = new URL(req.url);
+    const uid = searchParams.get("uid");
+
+    if (!uid) {
+      return NextResponse.json({
+        success: false,
+        message: "Account id is mandatory",
+      });
+    }
+
+    const accounts = await Account.find({ uid });
+
+    return NextResponse.json({ success: true, accounts });
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+}
+
+// Delete all acount
+export async function DELETE(req: Request) {
+  try {
+    await connectToDatabase();
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json({
+        success: false,
+        message: "Account id is mandatory",
+      });
+    }
+
+    await Account.findByIdAndDelete(id);
+
+    return NextResponse.json({
+      success: true,
+      message: "Account deleted successfully",
+    });
   } catch (err) {
     return NextResponse.json({
       success: false,
